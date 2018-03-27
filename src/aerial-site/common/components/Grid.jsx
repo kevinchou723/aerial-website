8861cebfd41b5a1cbc5e73b8480957267306e319
Kevin Chou <kchou723@gmail.com>
2018-03-26 22:56:11 -0400

    clean up some code

--- src/aerial-site/common/components/Grid.jsx
+++ src/aerial-site/common/components/Grid.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import injectSheet from 'react-jss';
-import pure from 'recompose/pure';
-import compose from 'recompose/compose';
+import nanoid from 'nanoid';
 import GridList, { GridListTile } from 'material-ui/GridList';
 import Dimensions from 'react-dimensions'
 
@@ -24,68 +23,67 @@ const styleSheet = theme => ({
     }
 });
 
-const Grid = ({ classes, images, isMobile, containerWidth }) => {
+// Had to make this a class since stateless components shouldn't use refs
+// which the dimensions library is using
+class Grid extends PureComponent {
+    render(){
+        const { classes, images, isMobile, containerWidth } = this.props;
+        const cellHeight = isMobile ? window.innerWidth : (containerWidth / 4);
+        const columns = isMobile ? 1 : 4;
+        return (
+            <div className={classes.gridContainer}>
+                <GridList cellHeight={cellHeight} className={classes.gridList} spacing={20} cols={columns}>
+                    {
+                        images.map(img => {
 
-    const cellHeight = isMobile ? window.innerWidth : (containerWidth/4);
-    const columns = isMobile ? 1 : 4;
-    
-    return (
-        <div className={classes.gridContainer}>
-            <GridList cellHeight={cellHeight} className={classes.gridList} spacing={20} cols={columns}>
-                {
-                    images.map(img => {
+                            //sorry for this ugly code, but this is to make grids within grids. Temporary
+                            if (img.hasSubGrid) {
+                                return (
+                                    <GridListTile component='ul' key={nanoid()} rows={img.rows || 1} cols={img.cols || 1}>
+                                        {
+                                            img.innerImgs.map((img) => {
+                                                return (
+                                                    <GridListTile
+                                                        key={nanoid()}
+                                                        rows={img.rows || 1}
+                                                        cols={img.cols || 1}
+                                                        style={{
+                                                            height: img.height,
+                                                            padding: img.padding
+                                                        }}
+                                                    >
+                                                        <img src={img.src} alt='img' />
+                                                    </GridListTile>
+                                                );
+                                            })
+                                        }
+                                    </GridListTile>
+                                );
+                            }
 
-                        //sorry for this ugly ass code, but this is to make grids within grids. Temporary
-                        if(img.hasSubGrid){
+                            if (img.hasBgWrapper) {
+                                return (
+                                    <GridListTile style={{
+                                        height: 'auto'
+                                    }} key={nanoid()} rows={img.rows || 1} cols={img.cols || 1}>
+                                        <div className={classes.subGridTile}>
+                                            <img className={classes.subGridImg} src={img.src} alt='img' />
+                                        </div>
+                                    </GridListTile>
+                                );
+                            }
+
                             return (
-                                <GridListTile key={Math.floor(Math.random() * 9999)} rows={img.rows || 1} cols={img.cols || 1}>
-                                    {
-                                        img.innerImgs.map((img) => {
-                                            return (
-                                                <GridListTile 
-                                                    key={Math.floor(Math.random() * 9999)}
-                                                    rows={img.rows || 1}
-                                                    cols={img.cols || 1}
-                                                    style={{
-                                                        height: img.height,
-                                                        padding: img.padding
-                                                    }}
-                                                >
-                                                    <img src={img.src} alt='img' />
-                                                </GridListTile>
-                                            );
-                                        })
-                                    }
+                                <GridListTile key={nanoid()} rows={img.rows || 1} cols={img.cols || 1}>
+                                    <img src={img.src} alt='img' />
                                 </GridListTile>
-                            );
-                        }
-
-                        if(img.hasBgWrapper){
-                            return (
-                                <GridListTile style={{
-                                    height: 'auto'
-                                }} key={Math.floor(Math.random() * 9999)} rows={img.rows || 1} cols={img.cols || 1}>
-                                    <div className={classes.subGridTile}>
-                                        <img className={classes.subGridImg} src={img.src} alt='img' />
-                                    </div>
-                                </GridListTile>
-                            );
-                        }
-                        
-                        return (
-                            <GridListTile key={Math.random()} rows={img.rows || 1} cols={img.cols || 1}>
-                                <img src={img.src} alt='img' />
-                            </GridListTile>
-                        )
-                    })
-                }
-            </GridList>
-        </div>
-    );
+                            )
+                        })
+                    }
+                </GridList>
+            </div>
+        );
+    }
 }
 
-
-export default compose(
-    pure,
-    injectSheet(styleSheet),
-)(Dimensions()(Grid));
+export default injectSheet(styleSheet)(Dimensions()(Grid));
